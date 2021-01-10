9408aaf157d96f534ee0878920c37323b48e79d7
YAT <yat@yat-net.com>
2021-01-10 11:37:17 +0900

    first-commit

---
+++ webpack.config.js
@@ -0,0 +1,111 @@
+const debug              = process.env.NODE_ENV !== 'production',
+	webpack              = require( 'webpack' ),
+	path                 = require( 'path' ),
+	Fiber                = require( 'fibers' ),
+	MiniCssExtractPlugin = require( 'mini-css-extract-plugin' ),
+	CopyWebpackPlugin    = require( 'copy-webpack-plugin' );
+
+module.exports = {
+	context: path.join( __dirname, 'src' ),
+	entry: './js/index.js',
+	devtool: 'source-map',
+	module: {
+		rules: [
+			//babel
+			{
+				test: /\.jsx?$/,
+				exclude: /( node_modules | bower_components )/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: [ '@babel/preset-react', '@babel/preset-env' ]
+						}
+					}
+				]
+			},
+			//scss
+			{
+				test: /\.(sa|sc|c)ss$/,
+				exclude: /( node_modules | bower_components )/,
+				use: [
+					{
+						loader: 'style-loader'
+						// loader: MiniCssExtractPlugin.loader
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							url: false,
+							sourceMap: true,
+							importLoaders: 2
+						}
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: {
+								plugins: [
+									// Autoprefixerを有効化
+									// ベンダープレフィックスを自動付与する
+									['autoprefixer', { grid: true }],
+								],
+							},
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							implementation: require('sass'),
+							fiber: Fiber,
+							sourceMap: true
+						}
+					}
+				]
+			},
+			//images
+			{
+				test: /\.( gif|png|jpg|eot|wof|woff|ttf|svg )$/,
+				// type: "asset/inline",
+			}
+		]
+	},
+	output: {
+		path: path.join( __dirname, '/src/' ),
+		filename: 'index.min.js',
+		publicPath: '/'
+	},
+	devServer: {
+		historyApiFallback: true,
+		contentBase       : path.join(__dirname, 'src'),
+		watchContentBase  : true,
+	},
+	plugins: debug ?
+	[
+		new MiniCssExtractPlugin( { filename: 'css/style.css' } ),
+		new CopyWebpackPlugin( {
+			patterns: [ {
+				from: '**/*.html',
+				// to: '/public/',
+				context: "./",
+			} ]
+		} )
+	]
+	:
+	[
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.optimize.UglifyJsPlugin( { mangle: false, soucemap: true }),
+		// new MiniCssExtractPlugin( { filename: 'css/style.css' } ),
+		// new CopyWebpackPlugin( {
+		// 	patterns: [ {
+		// 		from: '**/*.html',
+		// 		// to: '/public/',
+		// 		context: "./",
+		// 	} ]
+		// } )
+	],
+	resolve: {
+		modules: [ path.join( __dirname, 'src' ), 'node_modules' ],
+		extensions: [ '.js', '.jsx' ]
+	}
+}
